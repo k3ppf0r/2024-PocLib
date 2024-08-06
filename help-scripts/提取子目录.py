1fd5c4d70c7acbb88f325df21f7a819aa922a7e8
k3ppf0r <2094289254@qq.com>
2024-08-06 17:17:38 +0800

    py update

--- help-scripts/提取子目录.py
+++ help-scripts/提取子目录.py
@@ -1,12 +1,15 @@
 import os  
   
 def find_2024_directories(root_dir):  
+    count = 0
     for item in os.listdir(root_dir):  
         full_path = os.path.join(root_dir, item)  
         if os.path.isdir(full_path) and item.startswith('2024'):  
             print("- "+ item)  # 打印目录名称  
             for filename in os.listdir(full_path):  
                 print(f"  - {filename}")  
+                count += 1
+    print("poc count: ", count)
   
 if __name__ == "__main__":  
     root_directory = os.path.dirname(__file__)  
